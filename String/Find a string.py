5bce7785989c2b0793382247bf906173d896bbd2
Shamsullo <34841262+Shamsullo@users.noreply.github.com>
2017-12-29 12:39:06 +0500

    Create Find a string.py

---
+++ String/Find a string.py
@@ -0,0 +1,19 @@
+def count_substring(string, sub_string):
+    
+    count = 0
+    
+    for i in range(0, len(string)):
+        temp = string[i:i + len(sub_string)]
+        if (temp == sub_string):
+            count += 1
+        
+    return count
+    
+    
+    #given
+    if __name__ == '__main__':
+    string = raw_input().strip()
+    sub_string = raw_input().strip()
+    
+    count = count_substring(string, sub_string)
+    print count
