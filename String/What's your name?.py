11f1736a62a5d3ed70c84aefb3bb0a55210793c8
Shamsullo <34841262+Shamsullo@users.noreply.github.com>
2017-12-29 12:06:12 +0500

    Create What's your name?.py

---
+++ String/What's your name?.py
@@ -0,0 +1,9 @@
+def print_full_name(a, b):
+    print "Hello " + first_name + " " + last_name + "! You just delved into python."
+    
+    
+    #what's give
+    if __name__ == '__main__':
+    first_name = raw_input()
+    last_name = raw_input()
+    print_full_name(first_name, last_name)
