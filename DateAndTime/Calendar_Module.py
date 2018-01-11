26187d68f552dd4b191a4ac27d753d40b5d34ff7
Shamsullo <34841262+Shamsullo@users.noreply.github.com>
2018-01-11 18:08:04 +0000

    Update Calendar_Module.py

--- DateAndTime/Calendar_Module.py
+++ DateAndTime/Calendar_Module.py
@@ -3,8 +3,11 @@ import datetime
 
 input_date = input()
 
-month,day,year=map(int,input_date.split())
+month,day,year = map(int,input_date.split())
 
 day_of_the_week = datetime.date(year,month,day)
 
-print(calendar.day_name[day_of_the_week.weekday()].upper())
+print calendar.day_name[day_of_the_week.weekday()].upper()
+
+#the code isn't giving an expected result, i wonder why?
+
